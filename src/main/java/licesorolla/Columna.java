b4115eaf27efc600fb5b9634381bb00619ea9610
David <David@192.168.1.194>
2020-03-27 16:54:14 +0100

    Clases

---
+++ src/main/java/licesorolla/Columna.java
@@ -0,0 +1,5 @@
+package main.java.licesorolla;
+
+public class Columna {
+
+}
---
+++ src/main/java/licesorolla/Start.java
@@ -0,0 +1,10 @@
+package main.java.licesorolla;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
