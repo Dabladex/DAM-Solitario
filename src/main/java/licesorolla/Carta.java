2124e02bca58e5defe37ff5278c4469b035e9e9f
David <David@192.168.1.194>
2020-03-27 18:05:22 +0100

    Atributos carta y clase enum

--- src/main/java/licesorolla/Carta.java
+++ src/main/java/licesorolla/Carta.java
@@ -1,5 +1,111 @@
 package main.java.licesorolla;
 
 public class Carta {
+	
+	private numeroCarta num;
+	private int valorCarta;
+	
+	public Carta(numeroCarta num) {
+		this.num = num;
+		valorCarta();
+	}
+
+	public numeroCarta getNum() {
+		return num;
+	}
+
+	public void setNum(numeroCarta num) {
+		this.num = num;
+	}
+
+	public int getValorCarta() {
+		return valorCarta;
+	}
+
+	public void setValorCarta(int valorCarta) {
+		this.valorCarta = valorCarta;
+	}
+	
+	public void valorCarta() {
+
+		switch (num) {
+
+		case uno:
+
+			this.valorCarta = 1;
+
+			break;
+			
+		case dos:
+
+			this.valorCarta = 2;
+
+			break;
+
+		case tres:
+
+			this.valorCarta = 3;
+			
+			break;
+			
+		case cuatro:
+
+			this.valorCarta = 4;
+
+			break;
+		case cinco:
+
+			this.valorCarta = 5;
+
+			break;
+
+		case seis:
+
+			this.valorCarta = 6;
+
+			break;
+			
+		case siete:
+
+			this.valorCarta = 7;
+
+			break;
+		case ocho:
+
+			this.valorCarta = 8;
+
+			break;
+		case nueve:
+
+			this.valorCarta = 9;
+
+			break;
+		case diez:
+
+			this.valorCarta = 10;
+
+			break;
+			
+		case J:
+
+			this.valorCarta = 11;
+
+			break;
+			
+		case Q:
+
+			this.valorCarta = 12;
+
+			break;
+			
+		case K:
+
+			this.valorCarta = 13;
+
+			break;
+
+		}
+		
+	}
 
 }
---
+++ src/main/java/licesorolla/numeroCarta.java
@@ -0,0 +1,7 @@
+package main.java.licesorolla;
+
+public enum numeroCarta {
+	
+	uno, dos,tres,cuatro,cinco,seis,siete,ocho,nueve,diez, J, Q, K
+
+}
