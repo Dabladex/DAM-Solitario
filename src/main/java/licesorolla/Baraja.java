191870c76c09a58260a2b57314930da1cec39be6
David <David@192.168.1.194>
2020-03-30 18:21:39 +0200

    Generar baraja y barajar

--- src/main/java/licesorolla/Baraja.java
+++ src/main/java/licesorolla/Baraja.java
@@ -1,5 +1,62 @@
 package main.java.licesorolla;
 
+import java.util.ArrayList;
+
 public class Baraja {
+	
+	private ArrayList<Carta> baraja = new ArrayList<Carta>();
+	
+	public Baraja() {
+		generarBaraja();
+	}
+
+	public ArrayList<Carta> getBaraja() {
+		return baraja;
+	}
+
+	public void setBaraja(ArrayList<Carta> baraja) {
+		this.baraja = baraja;
+	}
+
+
+	private void generarBaraja() {
+
+		for (int i = 0; i < 8; i++) {
+			for (numeroCarta num : numeroCarta.values()) {
+				baraja.add(new Carta(num));
+			}
+		}
+
+	}
+	
+	public void barajar() {
+		
+		ArrayList<Carta> barajaOr = new ArrayList();
+		int numeroAle = 0;
+		while(baraja.size()!=0) {
+
+			numeroAle = (int) (Math.random() * baraja.size());
+			barajaOr.add(baraja.get(numeroAle));
+			baraja.remove(numeroAle);
+
+		}
+		
+		while(barajaOr.size()!=0) {
+			baraja.add(barajaOr.get(0));
+			barajaOr.remove(0);
+
+		}
+
+	}
+	
+public static void mostrar(ArrayList<Carta> baraja) {
+		
+		
+		for(int i = 0; i < baraja.size(); i++) {
+			
+			System.out.println(baraja.get(i).getValorCarta());
+		}
+		
+	}
 
 }
--- src/main/java/licesorolla/Start.java
+++ src/main/java/licesorolla/Start.java
@@ -1,10 +1,18 @@
 package main.java.licesorolla;
 
+import java.util.ArrayList;
+
 public class Start {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Baraja baraja = new Baraja();
+		
+		//generarBaraja(baraja);
+		//mostrar(baraja);
+		baraja.barajar();
 
 	}
+	
+	
 
 }
