a196e52fd09e6d78a6be826535706b38f134f05b
David <David@192.168.1.194>
2020-03-31 18:13:15 +0200

    para mario 1.2 :)

---
+++ src/main/java/licesorolla/Columna.java
@@ -0,0 +1,28 @@
+package main.java.licesorolla;
+
+import java.util.ArrayList;
+
+public class Columna {
+	
+	private ArrayList<Carta> columna = new ArrayList<Carta>();
+
+	public Columna() {
+
+		
+	}
+
+	public ArrayList<Carta> getColumna() {
+		return columna;
+	}
+
+	public void setColumna(ArrayList<Carta> columna) {
+		this.columna = columna;
+	}
+	
+	private void rellenarColumna() {
+		
+		
+		
+	}
+	
+}
